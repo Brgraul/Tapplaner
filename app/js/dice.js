446f2c2df55ba517636e0804f5052bdb9a5bbce7
Raúl Berganza <brgraul@gmail.com>
2017-06-16 17:54:59 +0300

    lil dice

--- app/js/dice.js
+++ app/js/dice.js
@@ -1,7 +1,8 @@
 var face=1;
 
 var show = function() {
-  document.getElementById("cube").setAttribute('class', 'show'+face);
+  document.getElementById("cube1").setAttribute('class', 'show'+face);
+  document.getElementById("cube2").setAttribute('class', 'show'+face);
   if(face==6) {
     face=1;
   } else {
